e40ae63fdd51eaf4ec0998e37ce68dd730b2f518
Fateen <fateen.najib@ui.ac.id>
2021-10-11 20:53:21 +0700

    Tutam_Fateen Najib Indramustika_2006468522_Modul 5

--- src/FateenJmartFH/Algorithm.java
+++ src/FateenJmartFH/Algorithm.java
@@ -1,11 +1,44 @@
 package FateenJmartFH;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 public class Algorithm {
     private Algorithm(){
 
     }
+
+    public static <T> List<T> collect(T[] array , T value){
+        final Iterator<T>it = Arrays.stream(array).iterator();
+        return collect(it, value);
+    }
+
+    public static <T> List<T> collect(Iterable<T>iterable , T value){
+        final Iterable<T>e = (Iterable<T>) iterable.iterator();
+        return collect(e, value);
+    }
+
+    public static <T> List<T> collect(Iterator<T>iterator , T value){
+        final Predicate<T> predicate = value::equals;
+        return collect(iterator, value);
+    }
+
+    public static <T> List<T> collect(T[] array, Predicate<T> pred){
+        final Iterator<T> itArray = Arrays.stream(array).iterator();
+        return collect(itArray, pred);
+    }
+
+    public static <T> List<T> collect(Iterator<T>iterator, Predicate<T> pred) {
+        int count = 0;
+        while(iterator.hasNext()){
+            count++;
+        }
+        final Predicate<T> predicate = pred::equals;
+        return collect(iterator, pred);
+    }
+
     public static <T> int count(T[] array,T value){
 
         return array.length;
@@ -158,13 +191,63 @@ public class Algorithm {
         return null;
     }
 
-    public static <T> T max(T first, T second) {
-        if(first.hashCode()>second.hashCode()){
-            return first;
-        }else {
-            return second;
+
+
+    public static <T> T max(T[] array){
+        int max = 0;
+        T maxT = null;
+        for (T t : array) {
+            if (t.hashCode() > max) {
+                max = t.hashCode();
+                maxT = t;
+            }
         }
+        return maxT;
     }
+
+    public static <T> T max(Iterable<T> iterable){
+        int max = 0;
+        T maxT = null;
+        for (T t : iterable) {
+            if (t.hashCode() > max) {
+                max = t.hashCode();
+                maxT = t;
+            }
+        }
+        return maxT;
+    }
+
+    public static <T> T max(Iterator<T> iterator){
+        int max = 0;
+        T maxT = null;
+        while (iterator.hasNext()) {
+            if (iterator.next().hashCode() > max) {
+                max = iterator.next().hashCode();
+                maxT = iterator.next();
+            }
+        }
+        return maxT;
+    }
+
+    public static <T> T max(T first, T second, Comparator<?super T> comparator){
+        int result = ((Comparable<T>) first).compareTo(second);
+        return result < -1? second : first;
+    }
+
+    public static <T> T max(T[] array, Comparator<?super T> comparator){
+        return null;
+    }
+
+    public static <T> T max(Iterable<T> iterable, Comparator<?super T> comparator){
+        T maxT = null;
+        for(T t : iterable) {
+            if (((Comparable<T>) maxT).compareTo(t) < 0) {
+                maxT = t;
+            }
+        }
+        return maxT;
+    }
+
     public static <T> T min(T first, T second) {
         if(first.hashCode()>second.hashCode()){
             return second;
@@ -173,4 +256,43 @@ public class Algorithm {
         }
     }
 
+    public static <T> T min(T[] array){
+        int min = array[0].hashCode();
+        T minT = null;
+        for (T t : array) {
+            if (t.hashCode() > min) {
+                min = t.hashCode();
+                minT = t;
+            }
+        }
+        return minT;
+    }
+
+    public static <T> T min(Iterable<T> iterable){
+        int min = iterable.hashCode();
+        T minT = null;
+        for (T t : iterable) {
+            if (t.hashCode() > min) {
+                min = t.hashCode();
+                minT = t;
+            }
+        }
+        return minT;
+    }
+
+    public static <T> T min(Iterator<T> iterator){
+        int min = iterator.next().hashCode();
+        T minT = null;
+        while (iterator.hasNext()) {
+            if (iterator.next().hashCode() > min) {
+                min = iterator.next().hashCode();
+                minT = iterator.next();
+            }
+        }
+        return minT;
+    }
+
+    public static <T extends Comparable<?super T>> T min(T first, T second, Comparator<?super T> comparator){
+        return null;
+    }
 }
